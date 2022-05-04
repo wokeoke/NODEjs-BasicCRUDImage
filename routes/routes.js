4cf998380a5c2d40a67d59f86e2464e9ee92881b
wokeoke <woke.oke@gmail.com>
2022-05-04 14:02:52 +0700

    EJS

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-  res.send('All Users');
+router.get('/', (req, res) => {
+  res.render('index.ejs', { title: 'Home Page' });
 });
 
 module.exports = router;
