0549761927569bfbc39f6fc71f8e24f62e257810
wokeoke <woke.oke@gmail.com>
2022-05-04 14:24:42 +0700

    Add Users Page

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// HOME PAGE
 router.get('/', (req, res) => {
   res.render('index.ejs', { title: 'Home Page' });
+});
+
+// ADD USER PAGE
+router.get('/add', (req, res) => {
+  res.render('add_users.ejs', { title: 'Add Users' });
 });
 
 module.exports = router;
